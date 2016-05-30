d4e5e4a94d8a8c5f3bb2e93caf62ee0422b0ec35
Zakaria Ridouh <zridouh@live.com>
2016-05-30 00:40:20 -0700

    force raleway font + clean

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,46 +3,44 @@ console.log("Easter Egg #1! Code: C0N501E");
 
 console.log("Easter Egg hint: I open at the close.");
 //Vive L'Algerie!
+
 var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
-	jQuery('#snitch').fadeIn(500, function() {
-		window.setTimeout(function() {
-			jQuery('#snitch').hide();
-		}, 5000);
-	}, "konami-code");
+  jQuery('#snitch').fadeIn(500, function() {
+    window.setTimeout(function() {
+      jQuery('#snitch').hide();
+    }, 5000);
+  }, "konami-code");
 }).listen();
-//WOW!
 new WOW().init();
-// Snackbar
 (function() {
-	'use strict';
-	var snackbarContainer = document.querySelector('#demo-toast-example');
-	var showToastButton = document.querySelector('#demo-show-toast');
-	showToastButton.addEventListener('click', function() {
-		'use strict';
-		var data = {
-			message: 'Console.Log U Hax0r'
-		};
-		snackbarContainer.MaterialSnackbar.showSnackbar(data);
-	});
+  'use strict';
+  var snackbarContainer = document.querySelector('#demo-toast-example');
+  var showToastButton = document.querySelector('#demo-show-toast');
+  showToastButton.addEventListener('click', function() {
+    var data = {
+      message: 'Console.Log U Hax0r'
+    };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+  });
 }());
 
 //Scroll to top
 $(document).ready(function() {
 
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
     } else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	//Click event to scroll to top
-	$('.scrollToTop').click(function() {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 850);
-		return false;
-	});
+      $('.scrollToTop').fadeOut();
+    }
+  });
+  //Click event to scroll to top
+  $('.scrollToTop').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 850);
+    return false;
+  });
 
 });
